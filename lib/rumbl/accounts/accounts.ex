8742afa4bf3156e1065bfd058391d424e4f5f435
Neil Scheuermann <neilscheuermann@gmail.com>
2020-05-29 16:52:20 -0600

    Finishes chpt 4 - Ecto Changesets

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -8,6 +8,16 @@ defmodule Rumbl.Accounts do
     Repo
   }
 
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
   def get_user(id) do
     Repo.get(User, id)
   end
