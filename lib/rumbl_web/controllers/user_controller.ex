00eae3d4860f3522ace959eebe7fc728b80e4363
Neil Scheuermann <neilscheuermann@gmail.com>
2020-06-01 21:27:25 -0600

    Generates video context, controller, and view

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -6,7 +6,7 @@ defmodule RumblWeb.UserController do
     Accounts.User
   }
 
-  plug :authenticate when action in [:index, :show]
+  plug :authenticate_user when action in [:index, :show]
 
   def create(conn, %{"user" => user_params}) do
     case Accounts.register_user(user_params) do
@@ -34,17 +34,5 @@ defmodule RumblWeb.UserController do
   def show(conn, %{"id" => id}) do
     user = Accounts.get_user(id)
     render(conn, "show.html", user: user)
-  end
-
-  # function plug
-  defp authenticate(conn, _opts) do
-    if conn.assigns.current_user do
-      conn
-    else
-      conn
-      |> put_flash(:error, "You must be logged in to access that page")
-      |> redirect(to: Routes.page_path(conn, :index))
-      |> halt()
-    end
   end
 end
