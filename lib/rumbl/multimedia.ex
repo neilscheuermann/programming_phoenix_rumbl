8eb806bada0859048255f8f2f934da858f7d0776
Neil Scheuermann <neilscheuermann@gmail.com>
2020-06-06 12:58:22 -0600

    Finish chapter 6 - Generators and Relationships

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -5,20 +5,65 @@ defmodule Rumbl.Multimedia do
 
   import Ecto.Query, warn: false
   alias Rumbl.Repo
+  alias Rumbl.Accounts
 
   alias Rumbl.Multimedia.Video
 
   @doc """
-  Returns the list of videos.
+  Returns an `%Ecto.Changeset{}` for tracking video changes.
 
   ## Examples
 
-      iex> list_videos()
-      [%Video{}, ...]
+      iex> change_video(video)
+      %Ecto.Changeset{source: %Video{}}
 
   """
-  def list_videos do
-    Repo.all(Video)
+  def change_video(%Accounts.User{} = user, %Video{} = video) do
+    video
+    |> Video.changeset(%{})
+    |> put_user(user)
+  end
+
+  @doc """
+  Creates a video.
+
+  ## Examples
+
+      iex> create_video(%{field: value})
+      {:ok, %Video{}}
+
+      iex> create_video(%{field: bad_value})
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
+    %Video{}
+    |> Video.changeset(attrs)
+    |> put_user(user)
+    |> Repo.insert()
+  end
+
+  @doc """
+  Deletes a video.
+
+  ## Examples
+
+      iex> delete_video(video)
+      {:ok, %Video{}}
+
+      iex> delete_video(video)
+      {:error, %Ecto.Changeset{}}
+
+  """
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    from(v in Video, where: v.id == ^id)
+    |> user_videos_query(user)
+    |> Repo.one!()
+    |> preload_user()
   end
 
   @doc """
@@ -35,24 +80,28 @@ defmodule Rumbl.Multimedia do
       ** (Ecto.NoResultsError)
 
   """
-  def get_video!(id), do: Repo.get!(Video, id)
+  def get_video!(id), do: preload_user(Repo.get!(Video, id))
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+    |> preload_user()
+  end
 
   @doc """
-  Creates a video.
+  Returns the list of videos.
 
   ## Examples
 
-      iex> create_video(%{field: value})
-      {:ok, %Video{}}
-
-      iex> create_video(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
+      iex> list_videos()
+      [%Video{}, ...]
 
   """
-  def create_video(attrs \\ %{}) do
-    %Video{}
-    |> Video.changeset(attrs)
-    |> Repo.insert()
+  def list_videos do
+    Video
+    |> Repo.all()
+    |> preload_user()
   end
 
   @doc """
@@ -73,32 +122,19 @@ defmodule Rumbl.Multimedia do
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a video.
+  ##
+  # Private
+  #
 
-  ## Examples
-
-      iex> delete_video(video)
-      {:ok, %Video{}}
-
-      iex> delete_video(video)
-      {:error, %Ecto.Changeset{}}
-
-  """
-  def delete_video(%Video{} = video) do
-    Repo.delete(video)
+  defp preload_user(video_or_videos) do
+    Repo.preload(video_or_videos, :user)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking video changes.
+  defp put_user(changeset, user) do
+    Ecto.Changeset.put_assoc(changeset, :user, user)
+  end
 
-  ## Examples
-
-      iex> change_video(video)
-      %Ecto.Changeset{source: %Video{}}
-
-  """
-  def change_video(%Video{} = video) do
-    Video.changeset(video, %{})
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
   end
 end
