4a0a1e037c093c246d3ae9acfe612f63e1be0f45
Neil Scheuermann <neilscheuermann@gmail.com>
2020-06-22 14:42:43 -0600

    Add test for Auth plug

--- config/test.exs
+++ config/test.exs
@@ -16,3 +16,6 @@ config :rumbl, RumblWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+# Change the hashing level to speed up tests
+config :pbkdf2_elixir, :rounds, 1
